7cc8671332807284175d5c0d7f3a45c4b674c668
Maciej Bromirski <50320623+Nevalicjus@users.noreply.github.com>
2023-10-01 21:40:14 +0200

    fixed quotes

--- extension.js
+++ extension.js
@@ -1,28 +1,28 @@
-const vscode = require('vscode');
-const { exec } = require('child_process');
-const path = require('path');
+const vscode = require("vscode");
+const { exec } = require("child_process");
+const path = require("path");
 
 let existingTerminal;
 
 function activate(context) {
-  let disposable = vscode.commands.registerCommand('extension.buildJava', () => {
+  let disposable = vscode.commands.registerCommand("extension.buildJava", () => {
     // Get the currently active text editor's file name
     const editor = vscode.window.activeTextEditor;
     if (!editor) {
-      vscode.window.showErrorMessage('No active text editor found.');
+      vscode.window.showErrorMessage("No active text editor found.");
       return;
     }
 
-    const javaPath = vscode.workspace.getConfiguration('nev-vs-java-builder').get('javaPath', '/opt/homebrew/opt/openjdk/bin');
+    const javaPath = vscode.workspace.getConfiguration("nev-vs-java-builder").get("javaPath", "/opt/homebrew/opt/openjdk/bin");
     
     const fileName = editor.document.fileName;
-    const fileBasenameNoExtension = path.basename(fileName, '.java');
+    const fileBasenameNoExtension = path.basename(fileName, ".java");
 
     // Check if an existing terminal window is open
     if (!existingTerminal) {
       // Create a new terminal with the correct working directory
       existingTerminal = vscode.window.createTerminal({
-        name: 'Nev Java Build',
+        name: "Nev Java Build",
         cwd: path.dirname(fileName) // Set the working directory to the directory containing the Java file
       });
     }
