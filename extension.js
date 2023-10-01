046583457a1f814c5e30216968f86dabecdc9e9b
Maciej Bromirski <50320623+Nevalicjus@users.noreply.github.com>
2023-10-01 21:39:21 +0200

    made terminal windows reusable

--- extension.js
+++ extension.js
@@ -2,6 +2,8 @@ const vscode = require('vscode');
 const { exec } = require('child_process');
 const path = require('path');
 
+let existingTerminal;
+
 function activate(context) {
   let disposable = vscode.commands.registerCommand('extension.buildJava', () => {
     // Get the currently active text editor's file name
@@ -11,20 +13,25 @@ function activate(context) {
       return;
     }
 
-    //const javaPath = vscode.workspace.getConfiguration().get('nev-vs-java-builder.javaPath'); 
     const javaPath = vscode.workspace.getConfiguration('nev-vs-java-builder').get('javaPath', '/opt/homebrew/opt/openjdk/bin');
     
     const fileName = editor.document.fileName;
     const fileBasenameNoExtension = path.basename(fileName, '.java');
 
-    // Open a new terminal with the correct working directory and run the script
-    const terminal = vscode.window.createTerminal({
-      name: 'Nev Java Build',
-      cwd: path.dirname(fileName) // Set the working directory to the directory containing the Java file
-    });
+    // Check if an existing terminal window is open
+    if (!existingTerminal) {
+      // Create a new terminal with the correct working directory
+      existingTerminal = vscode.window.createTerminal({
+        name: 'Nev Java Build',
+        cwd: path.dirname(fileName) // Set the working directory to the directory containing the Java file
+      });
+    }
 
-    terminal.show();
-    terminal.sendText(`${javaPath}/javac ${fileBasenameNoExtension}.java && ${javaPath}/java ${fileBasenameNoExtension} && rm ${fileBasenameNoExtension}.class`);
+    // Show the existing terminal
+    existingTerminal.show();
+    
+    // Run the script in the existing terminal
+    existingTerminal.sendText(`${javaPath}/javac ${fileBasenameNoExtension}.java && ${javaPath}/java ${fileBasenameNoExtension} && rm ${fileBasenameNoExtension}.class`);
   });
 
   context.subscriptions.push(disposable);
