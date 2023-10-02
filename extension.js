ee05ae0cb1216e5c0be4f16a2c699786c98f746e
Maciej Bromirski <50320623+Nevalicjus@users.noreply.github.com>
2023-10-03 00:37:42 +0200

    some fixes to how the filepath is obtained, and prep for dynamic submenus

--- extension.js
+++ extension.js
@@ -5,36 +5,51 @@ const path = require("path");
 let existingTerminal;
 
 function activate(context) {
-  let disposable = vscode.commands.registerCommand("extension.buildJava", () => {
-    // Get the currently active text editor's file name
-    const editor = vscode.window.activeTextEditor;
-    if (!editor) {
-      vscode.window.showErrorMessage("No active text editor found.");
+  let disposable_buildJava = vscode.commands.registerCommand("extension.buildJava", (uri) => {
+    if (!uri) {
+      vscode.window.showErrorMessage("No file path provided.");
       return;
     }
 
+    const filePath = uri.path;
+
     const javaPath = vscode.workspace.getConfiguration("nevs-affogato").get("javaPath", "/opt/homebrew/opt/openjdk/bin");
-    
-    const fileName = editor.document.fileName;
-    const fileBasenameNoExtension = path.basename(fileName, ".java");
+
+    const fileBasenameNoExtension = path.basename(filePath, ".java");
 
     // Check if an existing terminal window is open
+    let existingTerminal = vscode.window.terminals.find(
+      (terminal) => terminal.name === "Affogato Java Build"
+    );
+
     if (!existingTerminal) {
       // Create a new terminal with the correct working directory
       existingTerminal = vscode.window.createTerminal({
         name: "Affogato Java Build",
-        cwd: path.dirname(fileName) // Set the working directory to the directory containing the Java file
+        cwd: path.dirname(filePath), // Set the working directory to the directory containing the Java file
       });
     }
 
     // Show the existing terminal
     existingTerminal.show();
-    
-    // Run the script in the existing terminal
+
+    // Run the script in the existing terminal using the provided file path
     existingTerminal.sendText(`${javaPath}/javac ${fileBasenameNoExtension}.java && ${javaPath}/java ${fileBasenameNoExtension} && rm ${fileBasenameNoExtension}.class`);
   });
+  // this will def get reworked if vscode gets dynamic submenus
+  /*let disposable_buildJavaMenu = vscode.commands.registerCommand("extension.buildJavaMenu", () => {
+    // Read and parse options from settings.json
+    const options = vscode.workspace.getConfiguration("nevs-affogato").get("javaPaths", []);
 
-  context.subscriptions.push(disposable);
+    // Generate a list of options from settings
+    const optionsList = options.map((option) => `${option.alias}: ${option.path}`).join("\n");
+
+    // Show the options in a custom panel
+    vscode.window.showInformationMessage(optionsList);
+  });*/
+
+  context.subscriptions.push(disposable_buildJava);
+  //context.subscriptions.push(disposable_buildJavaMenu);
 }
 
 exports.activate = activate;
